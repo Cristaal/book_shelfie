3eece41c653776fe2b674004db4cab89ee8201be
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-02 20:00:45 -0800

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+
+require("bundler/setup")
+Bundler.require(:default, :test)
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Author.destroy_all()
+    Title.destroy_all()
+    Genre.destroy_all()
+  end
+end
